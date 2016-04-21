4efb961fa3f30958195fb265b7864d6c2a64ce9a
Koung <ktn@mxv-solutions.de>
2016-04-21 09:37:54 +0200

    implement ui

--- src/main/java/com/kt/demo/home/HomeController.java
+++ src/main/java/com/kt/demo/home/HomeController.java
@@ -1,15 +1,47 @@
 package com.kt.demo.home;
 
 import java.security.Principal;
+import java.time.LocalDate;
+import java.util.Queue;
 
+import javax.validation.Valid;
+
+import org.apache.commons.collections4.queue.CircularFifoQueue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.kt.demo.currency.Currency;
+import com.kt.demo.currency.CurrencyRequest;
+import com.kt.demo.currency.CurrencyService;
 
 @Controller
 public class HomeController {
 	
+    @Autowired
+	CurrencyService currencyService;
+    
+    Queue<CurrencyRequest> lastRequests = new CircularFifoQueue<CurrencyRequest>(10);
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index(Principal principal) {
-		return principal != null ? "home/homeSignedIn" : "home/homeNotSignedIn";
+	public String home(Principal principal, ModelMap model) throws Exception {
+		if (principal == null) {
+			return "home/homeNotSignedIn";
+		}
+		model.addAttribute("name", principal);
+        model.addAttribute("lastRequests", lastRequests);
+        model.addAttribute("currencyRequest", new CurrencyRequest(Currency.USD, Currency.EUR, LocalDate.now()));
+        return "home/homeSignedIn";
 	}
+
+    @RequestMapping(value = "/queryRates", method = RequestMethod.POST)
+    public String queryRates(ModelMap model, @ModelAttribute("currencyRequest") @Valid CurrencyRequest request) throws Exception {
+		currencyService.updateRate(request);
+		lastRequests.add(request);
+        model.addAttribute("lastRequests", lastRequests);
+        return "home/homeSignedIn";
+    }
 }
